b5f6ea86e7b3f1ea0e8b758b209967e4b482fb56
Aito Hideki <aito.hideki@gmail.com>
2022-06-21 12:33:12 -0400

    feat(layout): added default layout with header

---
+++ src/components/main/connect.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    v-if="isHome"
+    class="button-connect"
+  >
+    {{ mock.connected ? mock.address : 'Connect' }}
+  </button>
+  <template v-else>
+    <button
+      class="button-connect--small"
+    >
+      {{ mock.connected ? 'Disconnect' : 'Connect' }}
+    </button>
+    <div class="order-count ml-8">
+      {{ mock.orderCount }}
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { mock } from '@/consts'
+
+const route = useRoute()
+const isHome = computed(() => route.name === 'Home')
+</script>
---
+++ src/components/main/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <span class="font-tenebras text-2xl md:text-3xl xl:text-5xl uppercase  mt-2 xl:mt-4 xl:mb-2 mr-auto">
+      Antonym
+    </span>
+    <main-connect />
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useScroll } from '@vueuse/core'
+import { mock } from '@/consts'
+
+const { y } = useScroll(document)
+</script>
