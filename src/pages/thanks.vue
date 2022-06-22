2c1025affd87f3b86d419e211d6827baf3ec1534
Aito Hideki <aito.hideki@gmail.com>
2022-06-22 16:33:46 -0400

    feat(thanks): added thanks page

---
+++ src/pages/thanks.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="page flex flex-col">
+    <div class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-black font-bold text-center my-auto uppercase">
+      Thank you for your order
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 768px) {
+  .home.page {
+    background-size: auto 90%;
+  }
+}
+</style>
+
+<route>
+{
+  "name": "Thanks"
+}
+</route>
