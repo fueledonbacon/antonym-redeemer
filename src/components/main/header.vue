ca8d60b2c9d21667f28db43c7d3b85acc4c60bf8
Aito Hideki <aito.hideki@gmail.com>
2022-06-22 16:33:35 -0400

    feat(header): updated main header

--- src/components/main/header.vue
+++ src/components/main/header.vue
@@ -1,11 +1,17 @@
 <template>
   <header
     class="header backdrop-filter duration-300"
-    :class="{ 'backdrop-blur-lg py-4': y }"
+    :class="{
+      'backdrop-blur-lg py-4': y,
+      'bg-white/50': y
+    }"
   >
-    <span class="font-tenebras text-2xl md:text-3xl xl:text-5xl uppercase  mt-2 xl:mt-4 xl:mb-2 mr-auto">
+    <router-link
+      class="font-tenebras text-2xl md:text-3xl xl:text-5xl uppercase  mt-2 xl:mt-4 xl:mb-2 mr-auto"
+      :to="{ name: 'Catalogue' }"
+    >
       Antonym
-    </span>
+    </router-link>
     <main-connect />
   </header>
 </template>
