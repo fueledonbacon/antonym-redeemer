17e1a36de2c76762e584ce5807d566558997c114
sdelvalle57 <sdelvalle57@gmail.com>
2022-07-08 09:44:28 -0500

    Fully tested materia contract

--- test/materia.js
+++ test/materia.js
@@ -31,16 +31,15 @@ describe("Materia ERC1155 Contract", function () {
         antonym = await Antonym.deploy("https://antonymnft.s3.us-west-1.amazonaws.com/json/")
         await Promise.all(await signers.map(async (s, i) => {
             if(i > 0 && i <= 11) {
-                await antonym.releaseReserve(s.address, 1000)}
+                await antonym.releaseReserve(s.address, 10)}
         }))
-        await antonym.releaseReserve(signers[1].address, 1000)
         await antonym.deployed();
 
         const Lib = await ethers.getContractFactory("VerifySignature");
         const lib = await Lib.deploy();
         await lib.deployed();
 
-        const Materia = await ethers.getContractFactory("Materia", {
+        const Materia = await ethers.getContractFactory("MateriaMock", {
             libraries: {
                 VerifySignature: lib.address,
             },
@@ -71,43 +70,68 @@ describe("Materia ERC1155 Contract", function () {
         
         //tries to mint where minting is not yet started
         await expect(
-            materia.connect(signers[1]).mintMateria(1,NULL_SIGNATURE)
+            materia.connect(signers[1]).mintMateria(10, NULL_SIGNATURE)
         ).to.be.revertedWith('Minting not yet started');
 
         //tries to mint where minting is paused
         await materia.allowMinting(false)
         await expect(
-            materia.connect(signers[1]).mintMateria(1,NULL_SIGNATURE)
+            materia.connect(signers[1]).mintMateria(10, NULL_SIGNATURE)
         ).to.be.revertedWith('Minting is Paused');
 
         //tries to mint where is finished
         await materia.allowMinting(true)
         await timeIncreaseTo(timestamp + 1*60*11)
         await expect(
-            materia.connect(signers[1]).mintMateria(1,NULL_SIGNATURE)
+            materia.connect(signers[1]).mintMateria(10, NULL_SIGNATURE)
         ).to.be.revertedWith('Minting already ended');
+    })
+
+    it("Checks signatures", async function() {
+        await timeIncreaseTo(timestamp + 60*5+10)
+
+        //signs token 1 and tries to mint token2
+        let messageHash = await materia.messageHash(signers[1].address, 1);
+        let signature = signers[0].signMessage(utils.arrayify(messageHash));
+        await expect(
+            materia.connect(signers[1]).mintMateria(2, signature)
+        ).to.be.revertedWith('Wrong signature');
+
+        //signer 2 signs token 1
+        messageHash = await materia.messageHash(signers[1].address, 1);
+        signature = signers[1].signMessage(utils.arrayify(messageHash));
+        await expect(
+            materia.connect(signers[1]).mintMateria(1, signature)
+        ).to.be.revertedWith('Wrong signature');
+
+        //signs for signer1 and signer2 sends tx
+        messageHash = await materia.messageHash(signers[1].address, 11);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
+        await expect(
+            materia.connect(signers[2]).mintMateria(11, signature)
+        ).to.be.revertedWith('Wrong signature');
     })
 
     it("Mints Materia", async function() {
         await timeIncreaseTo(timestamp + 60*5+10)
 
         //tries to mint where wrong signature
-        let messageHash = await materia.messageHash(signers[2].address, 999);
-        let signature = signers[0].signMessage(utils.arrayify(messageHash));
+        let messageHash = await materia.messageHash(signers[2].address, 1);
+        let wrongSignature = signers[0].signMessage(utils.arrayify(messageHash));
 
         await expect(
-            materia.connect(signers[1]).mintMateria(999, signature)
+            materia.connect(signers[1]).mintMateria(1, wrongSignature)
         ).to.be.revertedWith('Wrong signature');
 
         //tries to mint where not token owner
         await expect(
-            materia.connect(signers[1]).mintMateria(1001,NULL_SIGNATURE)
+            materia.connect(signers[1]).mintMateria(11, NULL_SIGNATURE)
         ).to.be.revertedWith('Not token owner');
 
-        messageHash = await materia.messageHash(signers[1].address, 999);
-        signature = signers[0].signMessage(utils.arrayify(messageHash));
+        messageHash = await materia.messageHash(signers[1].address, 1);
+        let signature = signers[0].signMessage(utils.arrayify(messageHash));
 
-        await materia.connect(signers[1]).mintMateria(999, signature);
+        await materia.connect(signers[1]).mintMateria(1, signature);
         let totalSupply = await materia.totalSupply(1);
         expect(totalSupply).to.be.equal(1);
 
@@ -116,65 +140,68 @@ describe("Materia ERC1155 Contract", function () {
         
         //tries to mint again with same tokenId
         await expect(
-            materia.connect(signers[1]).mintMateria(999, signature)
+            materia.connect(signers[1]).mintMateria(1, signature)
         ).to.be.revertedWith('Token already used');
 
-        messageHash = await materia.messageHash(signers[1].address, 1);
+        messageHash = await materia.messageHash(signers[1].address, 2);
         signature = signers[0].signMessage(utils.arrayify(messageHash));
-        await materia.connect(signers[1]).mintMateria(1, signature);
+
+        await materia.connect(signers[1]).mintMateria(2, signature);
         totalSupply = await materia.totalSupply(1);
         expect(totalSupply).to.be.equal(2);
         balance = await materia.balanceOf(signers[1].address, 1)
         expect(balance).to.be.equal(2)
-
-
-
     })
 
     it("Mints Prima Materia", async function() {
         await timeIncreaseTo(timestamp + 60*5+10)
 
         //tries to mint where wrong signature
-        let messageHash = await materia.messageHash(signers[2].address, 1);
-        let signature = signers[0].signMessage(utils.arrayify(messageHash));
-        const proof = getProof(merkleRoot, 1);
-        const invalidProof = getProof(merkleRoot2, 1);
+        let messageHash = await materia.messageHash(signers[2].address, 9);
+        let wrongSignature = signers[0].signMessage(utils.arrayify(messageHash));
+        const proof = getProof(merkleRoot, 10);
+        const invalidProof = getProof(merkleRoot2, 10);
 
          //tries to mint with wrong signature
         await expect(
-            materia.connect(signers[1]).mintPrimaMateria(1, signature, proof)
+            materia.connect(signers[1]).mintPrimaMateria(9, wrongSignature, proof)
         ).to.be.revertedWith('Wrong signature');
 
-        messageHash = await materia.messageHash(signers[1].address, 1);
-        signature = signers[0].signMessage(utils.arrayify(messageHash));
-
         //tries to mint where not token owner
+        messageHash = await materia.messageHash(signers[1].address, 20);
+        let signature = signers[0].signMessage(utils.arrayify(messageHash));
         await expect(
-            materia.connect(signers[1]).mintPrimaMateria(1001, signature, proof)
+            materia.connect(signers[1]).mintPrimaMateria(20, signature, proof)
         ).to.be.revertedWith('Not token owner');
 
         //tries to mint where not at least one Materia created
+        messageHash = await materia.messageHash(signers[2].address, 10);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
         await expect(
-            materia.connect(signers[1]).mintPrimaMateria(1, signature, proof)
+            materia.connect(signers[2]).mintPrimaMateria(10, signature, proof)
         ).to.be.revertedWith('A Materia should be created first');
 
         //create a Materia first
+        messageHash = await materia.messageHash(signers[1].address, 1);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
         await materia.connect(signers[1]).mintMateria(1, signature);
 
         //tries to mint where invalid merkle proof
+        messageHash = await materia.messageHash(signers[2].address, 10);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
         await expect(
-            materia.connect(signers[1]).mintPrimaMateria(1, signature, invalidProof)
+            materia.connect(signers[2]).mintPrimaMateria(10, signature, invalidProof)
         ).to.be.revertedWith('Invalid merkle proof');
 
-        await materia.connect(signers[1]).mintPrimaMateria(1, signature, proof);
+        await materia.connect(signers[2]).mintPrimaMateria(10, signature, proof);
         let totalSupply = await materia.totalSupply(2);
         expect(totalSupply).to.be.equal(1);
-        let balance = await materia.balanceOf(signers[1].address, 2)
+        let balance = await materia.balanceOf(signers[2].address, 2)
         expect(balance).to.be.equal(1)
         
         //tries to mint again with same tokenId
         await expect(
-            materia.connect(signers[1]).mintPrimaMateria(1, signature, proof)
+            materia.connect(signers[1]).mintPrimaMateria(10, signature, proof)
         ).to.be.revertedWith('Token already used');
     })
 
@@ -184,8 +211,8 @@ describe("Materia ERC1155 Contract", function () {
         //mint materias
         let messageHash = await materia.messageHash(signers[1].address, 1);
         let signature = signers[0].signMessage(utils.arrayify(messageHash));
-        const proof1 = getProof(merkleRoot, 1);
-        const proof2 = getProof(merkleRoot, 2);
+        const proof1 = getProof(merkleRoot, 10);
+        const proof2 = getProof(merkleRoot, 20);
 
          //tries to batch mint whre deadline not over
          await expect(
@@ -194,56 +221,73 @@ describe("Materia ERC1155 Contract", function () {
 
         //mint materias, (2)
         await materia.connect(signers[1]).mintMateria(1, signature);
-        messageHash = await materia.messageHash(signers[2].address, 1001);
+        messageHash = await materia.messageHash(signers[2].address, 11);
         signature = signers[0].signMessage(utils.arrayify(messageHash));
-        await materia.connect(signers[2]).mintMateria(1001, signature);
+        await materia.connect(signers[2]).mintMateria(11, signature);
 
         //mint primas(2)
-        messageHash = await materia.messageHash(signers[1].address, 1);
+        messageHash = await materia.messageHash(signers[2].address, 10);
         signature = signers[0].signMessage(utils.arrayify(messageHash));
-        await materia.connect(signers[1]).mintPrimaMateria(1, signature, proof1);
+        await materia.connect(signers[2]).mintPrimaMateria(10, signature, proof1);
 
-        messageHash = await materia.messageHash(signers[1].address, 2);
+        messageHash = await materia.messageHash(signers[3].address, 20);
         signature = signers[0].signMessage(utils.arrayify(messageHash));
-        await materia.connect(signers[1]).mintPrimaMateria(2, signature, proof2);
+        await materia.connect(signers[3]).mintPrimaMateria(20, signature, proof2);
 
         await timeIncreaseTo(timestamp + 1*60*11)
         
         //tries to mint more than max per token
         await expect(
-            materia.mintBatchMateria(signers[0].address, 9999, 1)
+            materia.mintBatchMateria(signers[0].address, 99, 1)
         ).to.be.revertedWith("Amount Materia exceeded")
 
         await expect(
-            materia.mintBatchMateria(signers[0].address, 1, 51)
+            materia.mintBatchMateria(signers[0].address, 1, 4)
         ).to.be.revertedWith("Amount Prima Materia exceeded")
 
 
-        await materia.mintBatchMateria(signers[0].address, 20, 5)
+        await materia.mintBatchMateria(signers[0].address, 98, 3)
         let totalSupply = await materia.totalSupply(1);
-        expect(totalSupply).to.be.equal(22);
+        expect(totalSupply).to.be.equal(100);
 
         totalSupply = await materia.totalSupply(2);
-        expect(totalSupply).to.be.equal(7);
+        expect(totalSupply).to.be.equal(5);
+
+
+        //tries to mint one more materia
+        await materia.setDeadline(timestamp + 1*60*60);
+        messageHash = await materia.messageHash(signers[2].address, 12);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
+        await expect(
+            materia.connect(signers[2]).mintMateria(12, signature)
+        ).to.be.revertedWith("Amount Materia exceeded")
+
+        //tries to mint one prima more materia
+        const proof3 = getProof(merkleRoot, 30);
+        messageHash = await materia.messageHash(signers[4].address, 30);
+        signature = signers[0].signMessage(utils.arrayify(messageHash));
+        await expect(
+            materia.connect(signers[4]).mintPrimaMateria(30, signature, proof3)
+        ).to.be.revertedWith("Amount Prima Materia exceeded")
 
 
     })
 })
 
 function merkleTree() {
-    //first 52 tokens will be 1/1 skin
+    //tokens 10, 20, 30, 40, 50
     let tokenPrimaMateriaIds = [];
-    for(let j = 0; j < 52; j++) {
-        tokenPrimaMateriaIds.push(hashData(j+1))
+    for(let j = 1; j <= 5; j++) {
+        tokenPrimaMateriaIds.push(hashData(j*10))
     }
     return new MerkleTree(tokenPrimaMateriaIds, utils.keccak256, { sortPairs: true });
 } 
 
 function merkleTree2() {
-    //token 52 to 103 tokens will be 1/1 skin
+    //tokens 9, 19, 29, 39, 49
     let tokenPrimaMateriaIds = [];
-    for(let j = 52; j < 104; j++) {
-        tokenPrimaMateriaIds.push(hashData(j+1))
+    for(let j = 1; j <= 5; j++) {
+        tokenPrimaMateriaIds.push(hashData(j*10-1))
     }
     return new MerkleTree(tokenPrimaMateriaIds, utils.keccak256, { sortPairs: true });
 } 
@@ -256,16 +300,5 @@ const hashData = (tokenId) => {
     return Buffer.from(ethers.utils.solidityKeccak256(['uint256'], [tokenId]).slice(2), 'hex')
 }
 
-async function signData(signer, account, tokenId) {
-    const encoded = ethers.utils.solidityKeccak256(["address", "uint256"], [account, tokenId]);
-    const signature = await signer.signMessage(encoded);
-    return signature
-}
 
-
-function toEthSignedMessageHash (messageHex) {
-    const messageBuffer = Buffer.from(messageHex.substring(2), 'hex');
-    const prefix = Buffer.from(`\u0019Ethereum Signed Message:\n${messageBuffer.length}`);
-    return web3.utils.sha3(Buffer.concat([prefix, messageBuffer]));
-}
 
