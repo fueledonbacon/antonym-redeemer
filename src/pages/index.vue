18a48770fe3d7ce74e480d9418c53082f417e0d4
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-07-09 18:05:09 -0500

    fix(index): removed connect to

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -59,7 +59,8 @@
         class="home__link flex items-center <sm:w-full justify-between animate-pulse ml-auto"
         :to="{ name: 'Intro' }"
       >
-        Connect to Begin Redemption
+   
+        Begin Redemption
         <img
           class="ml-2"
           src="/images/continue.svg"
