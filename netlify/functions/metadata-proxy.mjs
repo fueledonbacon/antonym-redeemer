d546bedb28f1c03f751a7015521b9af60244829c
Ryan Cwynar <ryan@cwynar.dev>
2022-07-15 17:55:04 -0500

    fix(metadata proxy): add headers to metadata proxy

--- netlify/functions/metadata-proxy.mjs
+++ netlify/functions/metadata-proxy.mjs
@@ -4,8 +4,20 @@ export const handler = async function (event, context) {
   const nftId = event.queryStringParameters.id;
   try {
     const nft = await find(nftId);
-    return { statusCode: 200, body: JSON.stringify(nft) };
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify(nft),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 500,
+      body: error.toString(),
+    };
   }
 };
