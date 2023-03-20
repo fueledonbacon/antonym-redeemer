a8dd7590a0703137546bd24c6da709966b4ecbcf
Byldr <ryan@fueledonbacon.com>
2023-03-20 14:50:33 -0400

    hotfix token status checker

--- netlify/functions/token-status.mjs
+++ netlify/functions/token-status.mjs
@@ -1,12 +1,27 @@
 
-import { findToken } from '../../common/token-handler.mjs'
+import { findToken, fetchNftById } from '../../common/token-handler.mjs'
 
 export const handler = async (event, context) => {
   const { tokenID } = JSON.parse(event.body)
-  const token = await findToken(tokenID)
+  const metadata = await fetchNftById(tokenID)
+  if(!metadata) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: 'Token not found' })
+    }
+  }
+  
+  const statusTrait = metadata.attributes.find((attr) => attr.trait_type === 'STATUS')
+
+  if(!statusTrait) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ redeemed: false })
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ redeemed: token.redeemed })
+    body: JSON.stringify({ redeemed: statusTrait.value === 'redeemed' })
   }
 }
